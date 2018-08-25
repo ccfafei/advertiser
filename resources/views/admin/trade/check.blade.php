fb88a1a24f4c9c8061a4a2f549ac2d1a35e52074
afei <chuhappyday@163.com>
2018-08-25 09:49:52 +0800

    no message

--- resources/views/admin/trade/check.blade.php
+++ resources/views/admin/trade/check.blade.php
@@ -295,7 +295,7 @@
             // });
 
             //选择审核，回款,出款处理等 icheckbox_minimal-blue
-            $('.grid-row-checkbox').iCheck({checkboxClass: 'icheckbox_minimal'}).on('ifChanged', function () {
+            $('.grid-row-checkbox').iCheck({checkboxClass: ''}).on('ifChanged', function () {
                 if (this.checked) {
                     $(this).closest('tr').css('background-color', '#ffffd5');
                 } else {
@@ -312,7 +312,7 @@
                 return selected;
             }
 
-            $('.grid-select-all').iCheck({checkboxClass: 'icheckbox_minimal'});
+            $('.grid-select-all').iCheck({checkboxClass: ''});
             $('.grid-select-all').on('ifChanged', function (event) {
                 if (this.checked) {
                     $('.grid-row-checkbox').iCheck('check');
